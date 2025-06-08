b4b135f3b1e0a6c81adad17f54c535c34cba081b
JuFML <julianafernandezlima@hotmail.com>
2025-06-08 08:03:28 +0200

    feat: pass products as props to ProductCard

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,15 +1,21 @@
 import { BsCartPlus } from "react-icons/bs"
 
+interface ProductCardProps {
+  title: string;
+  price: number;
+  cover: string
+}
 
-export const ProductCard = () => {
+
+export const ProductCard = ({ title, price, cover }: ProductCardProps) => {
   return (
-    <article className="w-full flex flex-col items-center">
-      <img className="rounded-lg max-h-70 mb-2"
-        src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
-        alt="Logo do produto" />
-      <p>Airpods Pro</p>
+    <article className="w-full flex flex-col">
+      <img className="rounded-lg max-h-70 mb-2 mx-auto"
+        src={cover}
+        alt={title} />
+      <p>{title}</p>
       <div className="flex gap-3 items-center">
-        <strong className="text-zinc-700/90">R$ 1000</strong>
+        <strong className="text-zinc-700/90">{price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</strong>
         <button className="bg-zinc-900 p-1 rounded">
           <BsCartPlus size={20} color="#FFF" />
         </button>
