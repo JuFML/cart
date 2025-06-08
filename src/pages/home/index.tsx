658f85419a651a5e4d128fe612f45b1f37f0f296
JuFML <julianafernandezlima@hotmail.com>
2025-06-08 06:31:21 +0200

    feat: add content to Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,14 @@
+import { ProductCard } from "../../components/productCard"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <main className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+        <section className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+          <ProductCard />
+        </section>
+      </main>
+    </>
   )
 }
