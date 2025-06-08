c486815be3f85232a95d913713e7dd95ab2297e3
JuFML <julianafernandezlima@hotmail.com>
2025-06-08 06:50:25 +0200

    feat: create ProductLine component

---
+++ src/components/productLine/index.tsx
@@ -0,0 +1,20 @@
+
+export const ProductLine = () => {
+  return (
+    <section className="flex items-center justify-between border-b-2 border-gray-300">
+      <img
+        src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
+        alt="Logo produto"
+        className="w-28"
+      />
+      <strong>Preço: R$1.000</strong>
+      <div className="flex items-center justify-center gap-3">
+        <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">-</button>
+        1
+        <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">+</button>
+      </div>
+
+      <strong>SubTotal: R$1.000</strong>
+    </section>
+  )
+}
